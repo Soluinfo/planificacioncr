bbbe27d8f03516686fa1cd77e1ef0192d6410446
DiegoIntriago <dintriago07@gmail.com>
2017-12-06 23:54:38 -0500

    cambio de prueba esperemos y funcione

--- application/views/plantilla/header.php
+++ application/views/plantilla/header.php
@@ -119,10 +119,10 @@
                     </li>
                     <li class="xn-title">Components</li>
                     <li class="xn-openable">
-                        <a href="#"><span class="fa fa-cogs"></span> <span class="xn-text">UI Kits</span></a>                        
+                        <a href="#"><span class="fa fa-cogs"></span> <span class="xn-text">Proyectos</span></a>                        
                         <ul>
-                            <li><a href="ui-widgets.html"><span class="fa fa-heart"></span> Widgets</a></li>                            
-                            <li><a href="ui-elements.html"><span class="fa fa-cogs"></span> Elements</a></li>
+                            <li><a href="ui-widgets.html"><span class="fa fa-heart"></span> Agregar Proyectos</a></li>                            
+                            <li><a href="ui-elements.html"><span class="fa fa-cogs"></span> Agregar Objetivos</a></li>
                             <li><a href="ui-buttons.html"><span class="fa fa-square-o"></span> Buttons</a></li>                            
                             <li><a href="ui-panels.html"><span class="fa fa-pencil-square-o"></span> Panels</a></li>
                             <li><a href="ui-icons.html"><span class="fa fa-magic"></span> Icons</a><div class="informer informer-warning">+679</div></li>
---
+++ application/views/proyectos/prueba.php
@@ -0,0 +1,62 @@
+<div class="panel-body">
+    <table id="customers2" class="table table-striped table-bordered table-hover table-condensed">
+
+        <thead>
+        <tr>
+        <th> <div class="col-md-8 col-xs-8">Proyecto</th>
+        <th> <div class="col-md-8 col-xs-8">En Progreso</th>
+        <th> <div class="col-md-8 col-xs-8">Retraso</th>
+        </tr>
+</thead>
+        <tbody>
+        <tr>
+            <div class="col-md-8 col-xs-12">
+            <td> <select class="form-control select">
+                    <option>Administrativa</option>
+                    <option>Financiera</option>
+                    <option>Recreativa</option>
+                    <option>Operacional</option>
+                    <option>Academica</option>
+                </select></td>
+                </div>
+            <td><div class="col-md-8 col-xs-8">Realizar djfhdjhfjdhfjdhfjdfhjdfhdjfhfkfjgkfjgkfjgkfjgkfjgkfjgkfgkgj</td>
+            <td>	Realizar kjfkdjfkdjfkdjfkdjfkdjfkdjfkdfj</td>
+        </tr>
+        <tr><td>
+            <div class="form-group">
+                <label class="col-sm-6 control-label">Actividades</label><br>
+                <div class="col-md-8 col-xs-12">
+                    <textarea class="form-control"></textarea>
+                </div>
+            </div>
+        </td></tr>
+        <tr><td>
+            <label class="col-sm-6 col-xs-6 ">Dirección </label><br>
+            <div class="col-md-8 col-xs-12">
+
+                 <select class="form-control">
+                        <option>Administrativa</option>
+                        <option>Financiera</option>
+                        <option>Recreativa</option>
+                        <option>Operacional</option>
+                        <option>Academica</option>
+                    </select></td>
+            </div>
+        </tr>
+<tr>
+    <td>
+<div class="form-group">
+    <label class="col-md-8 col-xs-12 control-label">Fecha de Creación </label><br>
+    <div class="col-md-8 col-xs-12">
+        <div class="input-group">
+            <span class="input-group-addon"><span class="fa fa-calendar"></span></span>
+            <input type="text" class="form-control datepicker" value="2014-11-01">
+
+</div>
+    </td></tr>
+        </tbody>
+
+
+       </table>
+
+</div>
